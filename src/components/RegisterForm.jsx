06293982837841b07c988175a61ec08648cf74bf
Mateo Santa <mateo.santamanrique@gmail.com>
2023-10-21 10:44:02 +0200

    register

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,14 +1,25 @@
+import {useEffect, useState} from 'react';
+
 
 const RegisterForm = () => {
-
-   /* const getData = async () => {
+    const [userData, setUserData] = useState([]); 
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+   const getData = async () => {
         try {
-            const getResponse =  await axios.get('http://localhost:3000/api/blogs/register');
-            console.log(getResponse);
+            const requestData = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ name: name, email: email, password: password })
+            };
+            const getResponse =  await fetch('http://localhost:3000/api/register', requestData);
+            const data = await getResponse.json();
+            console.log(data);
 
-            if(!getResponse) throw new Error(`Fetching Data failed, due to:`)
+            if(!data) throw new Error(`Fetching Data failed, due to:`)
 
-            setUserData(getResponse.data)
+            setUserData(data)
 
         } catch(error) {
             console.log(error.message)
@@ -17,21 +28,21 @@ const RegisterForm = () => {
 
     useEffect(() => {
        getData()
-    }, []);*/
+    }, []);
 
     return(
         <div>
-            <form className="form" action='/login' method="get">
-                <label for="name">Name:</label>
-                <input id="name" name="name" />
+            <form className="form" action='/login' method="post">
+                <label htmlFor="name">Name:</label>
+                <input id="name" name="name" onChange={()=>{setName}} required/>
                 <br />
-                <label for="email">Username:</label>
-                <input id="email" name="email" />
+                <label htmlFor="email">Email:</label>
+                <input id="email" name="email" onChange={()=>{setEmail}} required/>
                 <br />
-                <label for="password">Password:</label>
-                <input id="password" name="password" />
+                <label htmlFor="password">Password:</label>
+                <input id="password" name="password" onChange={()=>{setPassword}} required/>
                 <br />
-                <input type="submit" id="submit" value="Submit" />
+                <input type="submit" id="submit" value="Register" />
             </form>
         </div>
     )
